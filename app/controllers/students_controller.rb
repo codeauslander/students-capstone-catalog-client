3c0d87cfc6e543c63df678f209cf55b7107728fc
codeauslander <codeauslander@gmail.com>
2018-03-12 20:18:45 -0500

    second commit

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,16 +9,21 @@ class StudentsController < ApplicationController
       }
     ]
 
+    
+
     @students = ss 
     # @students = Student.all
-    render 'index.json.jbuilder'
+    puts @students
+    # render 'index.json.jbuilder'
+    render json: @students.as_json
   end
   def show
     s = {
-        id:2,first_name:"Cesar",last_name:"Catano",email: "cesar@gmail.com", phone_number: "1234", short_bio:"Awesome", linkedin_url:"my linkding_url", twitter_handle: "my twitter_handle", website_url: "my website_url", online_resume: "my online resume", github: "my github", photo: "my photo"
+        id:3,first_name:"Lara",last_name:":D",email: "lara@gmail.com", phone_number: "1234", short_bio:"Awesome", linkedin_url:"my linkding_url", twitter_handle: "my twitter_handle", website_url: "my website_url", online_resume: "my online resume", github: "my github", photo: "my photo"
     }
     @student = s
     # @student = Student.find(params[:id])
-    render 'show.json.jbuilder'
+    # render 'show.json.jbuilder'
+    render json: @student.as_json
   end
 end
